8e2869a51692dcbc8fb76a564007b5aa87cfe893
Abdelrahman <abdoumohamed96@gmail.com>
2017-12-18 20:59:24 +0200

    forgot to run rubocop

--- api/app/models/post.rb
+++ api/app/models/post.rb
@@ -3,5 +3,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :body, presence: true
-  validates :is_public, :inclusion => {:in => [true, false]}
+  validates :is_public, inclusion: { in: [true, false] }
 end
