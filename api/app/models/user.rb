d6141d6e5d97380c3ca8a0907d95518a1d030f30
Essam Ewaisha <essamemad16@gmail.com>
2017-12-22 21:52:48 +0200

    Added some user methods

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -13,9 +13,25 @@ class User < ActiveRecord::Base
   has_many :friends, :through => :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id", dependent: :destroy
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
-
   enum gender: %i[ male female lesbians male_lesbians homosexual
                    heterosexual bisexual prefer_not_to_specify]
   # we don't want to piss off emma watson
   enum relationship_status: %i[single married]
+
+
+  # Functions
+  def isFriendsWith(friend)
+    @friend = self.friendships.where(is_relationship_established:true, friend_id: friend)
+    if @friend.first.present?
+      return true
+    else
+      return self.inverse_friendships.where(is_relationship_established:true,user_id: friend).first.present?
+    end
+  end
+
+#Returns list of users that have requested friendship with current user
+def friendRequests
+  @ids = self.inverse_friendships.where(is_relationship_established: false).pluck(:user_id)
+  User.find(@ids)
+end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User',
                               at: 'auth',
                               skip: %i[omniauth_callbacks password]
+  get 'user/show'
+  get 'user/list'
 end
