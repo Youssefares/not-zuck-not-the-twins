46518fee6c5702227308c1df5e33ce9b86e54214
Youssefares <Youssefares@ymail.com>
2017-12-22 02:48:25 +0200

    Complete sign up form

--- view-app/src/components/Home/index.jsx
+++ view-app/src/components/Home/index.jsx
@@ -8,30 +8,28 @@ import SignUp from './SignUp';
 import SignIn from './SignIn';
 
 const Home = () => (
-  <div className="row">
-    <Grid>
-      <Grid.Row id="navbar">
-        <Grid.Column width={6} padded>
-          <h1 id="websitename">social-network-c137</h1>
-        </Grid.Column>
-        <Grid.Column width={10} padded>
-          <SignIn signIn={() => {}} />
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column width={6} padded>
-          <Image src="https://i.imgur.com/XOQHLsX.jpg" />
-          <div id="rickquote">
-            <h3> I turned myself into a social network, Morty!</h3>
-            <h1> I am social network rick! </h1>
-          </div>
-        </Grid.Column>
-        <Grid.Column width={10}>
-          <SignUp signUp={() => {}} />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </div>
+  <Grid>
+    <Grid.Row id="navbar">
+      <Grid.Column width={6} padded>
+        <h1 id="websitename">social-network-c137</h1>
+      </Grid.Column>
+      <Grid.Column width={10} padded>
+        <SignIn signIn={() => {}} />
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={6} padded>
+        <Image src="./turned_myself.png" />
+        <div id="rickquote">
+          <h3> I turned myself into a social network, Morty!</h3>
+          <h1> I am social network rick! </h1>
+        </div>
+      </Grid.Column>
+      <Grid.Column width={10}>
+        <SignUp signUp={() => {}} />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
 );
 
 // Home.propTypes = {
