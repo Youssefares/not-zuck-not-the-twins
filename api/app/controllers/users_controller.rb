9407d7509610f1ba1624257295f3983e87e5ccc4
Abdelrahman <abdoumohamed96@gmail.com>
2017-12-23 21:20:28 +0200

    change which posts to show in feed and hide private information for non friend users

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
-    @user = User.where(id:params[:id]).first
-    if @user.present?
+    @requested_user = User.where(id:params[:id]).first
+
+    if @requested_user.present?
+      # hide about me and birthdate if the requested user is not me and is not my friend
+      is_my_friend = current_user.friendships.where(friend_id: @requested_user.id, is_relationship_established: true).length > 0
+      
+      if current_user.id != @requested_user.id && !is_my_friend
+         @requested_user.about = nil
+        @requested_user.birthdate = nil
+      end
+    
       render json: {
         status: 200,
-        result: @user,
+        result: @requested_user,
         success: 1,
         message:''
       }.to_json
@@ -141,17 +152,24 @@ class UsersController < ApplicationController
     end
   end
 
-
   def feed
     @feed = []
     @user = User.find(params[:user_id])
+
+    # get my friends' private posts
     @user.friendships.each do |friendship|
       next unless friendship.is_relationship_established
 
       # TODO: merge arrays without making new copies
-      @feed += Post.where(user_id: friendship.friend_id, is_public: true)
+      @feed += Post.where(user_id: friendship.friend_id, is_public: false)
     end
 
+    # get all public posts (including my public posts and my friends' public posts)
+    @feed += Post.where(is_public: true)
+
+    # get my private posts
+    @feed += Post.where(user_id: @user.id, is_public: false)
+    
     @feed.sort!  { |a, b| b.updated_at <=> a.updated_at }
 
     render json: @feed
