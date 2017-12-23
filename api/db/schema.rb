80ce0b1f23ce1ff13634af7eb44810a58a6465e9
Essam Ewaisha <essamemad16@gmail.com>
2017-12-23 23:27:51 +0200

    Added image upload

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221094941) do
+ActiveRecord::Schema.define(version: 20171223201644) do
 
   create_table "friendships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.boolean "is_relationship_established", default: false
@@ -56,6 +56,10 @@ ActiveRecord::Schema.define(version: 20171221094941) do
     t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_file_name"
+    t.string "picture_content_type"
+    t.integer "picture_file_size"
+    t.datetime "picture_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
