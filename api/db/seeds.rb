b35d11af3e36f362695ade5688d340b25b4c27b1
Youssefares <Youssefares@ymail.com>
2017-12-23 16:22:13 +0200

    Fix seeds

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 # frozen_string_literal: true
 
 User.create!(
@@ -20,6 +11,6 @@ User.create!(
   ]
 )
 
-posts = Post.create([{body: "I am youssef", is_public: true, user_id: 2},
-					 {body: "I like potatoes", is_public: true, user_id: 3},
-					 {body: "just one more test case", is_public: true, user_id: 4}])
+Post.create!([{ body: 'I am youssef', is_public: true, user_id: 1 },
+              { body: 'I like potatoes', is_public: true, user_id: 1 },
+              { body: 'just one more test case', is_public: true, user_id: 3 }])
